1e93b345aac7824f30418441077b047af945a63c
sachintha00 <sachintha0603@gmail.com>
2021-12-02 08:53:33 +0530

    updated

--- HNCSchoolManagementSystem/HNCSchoolManagementSystem/frmAdminMain.Designer.cs
+++ HNCSchoolManagementSystem/HNCSchoolManagementSystem/frmAdminMain.Designer.cs
@@ -29,14 +29,16 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(frmAdminMain));
             this.elips = new Guna.UI.WinForms.GunaElipse(this.components);
             this.pnlMain = new Guna.UI2.WinForms.Guna2Panel();
+            this.pnlChangeWindow = new Guna.UI2.WinForms.Guna2Panel();
             this.btnClose = new Guna.UI2.WinForms.Guna2ControlBox();
             this.pnlSideMenu = new Guna.UI2.WinForms.Guna2Panel();
             this.guna2Button1 = new Guna.UI2.WinForms.Guna2Button();
             this.drag1 = new Guna.UI.WinForms.GunaDragControl(this.components);
             this.drag2 = new Guna.UI.WinForms.GunaDragControl(this.components);
-            this.pnlChangeWindow = new Guna.UI2.WinForms.Guna2Panel();
+            this.guna2Button2 = new Guna.UI2.WinForms.Guna2Button();
             this.pnlMain.SuspendLayout();
             this.pnlSideMenu.SuspendLayout();
             this.SuspendLayout();
@@ -58,6 +60,18 @@
             this.pnlMain.Size = new System.Drawing.Size(1330, 888);
             this.pnlMain.TabIndex = 0;
             // 
+            // pnlChangeWindow
+            // 
+            this.pnlChangeWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.pnlChangeWindow.BackColor = System.Drawing.Color.Transparent;
+            this.pnlChangeWindow.Location = new System.Drawing.Point(283, 52);
+            this.pnlChangeWindow.Name = "pnlChangeWindow";
+            this.pnlChangeWindow.ShadowDecoration.Parent = this.pnlChangeWindow;
+            this.pnlChangeWindow.Size = new System.Drawing.Size(1044, 833);
+            this.pnlChangeWindow.TabIndex = 3;
+            // 
             // btnClose
             // 
             this.btnClose.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
@@ -77,6 +91,7 @@
             // 
             // pnlSideMenu
             // 
+            this.pnlSideMenu.Controls.Add(this.guna2Button2);
             this.pnlSideMenu.Controls.Add(this.guna2Button1);
             this.pnlSideMenu.Dock = System.Windows.Forms.DockStyle.Left;
             this.pnlSideMenu.FillColor = System.Drawing.Color.FromArgb(((int)(((byte)(37)))), ((int)(((byte)(44)))), ((int)(((byte)(72)))));
@@ -93,14 +108,18 @@
             this.guna2Button1.Cursor = System.Windows.Forms.Cursors.Hand;
             this.guna2Button1.CustomImages.Parent = this.guna2Button1;
             this.guna2Button1.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 9F);
+            this.guna2Button1.Font = new System.Drawing.Font("Segoe UI", 10F);
             this.guna2Button1.ForeColor = System.Drawing.Color.White;
             this.guna2Button1.HoverState.BorderColor = System.Drawing.Color.Transparent;
             this.guna2Button1.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
             this.guna2Button1.HoverState.FillColor = System.Drawing.Color.Transparent;
-            this.guna2Button1.HoverState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button1.HoverState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.guna2Button1.HoverState.ForeColor = System.Drawing.Color.LightGray;
+            this.guna2Button1.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.HoverState.Image")));
             this.guna2Button1.HoverState.Parent = this.guna2Button1;
+            this.guna2Button1.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button1.Image")));
+            this.guna2Button1.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2Button1.ImageSize = new System.Drawing.Size(25, 25);
             this.guna2Button1.Location = new System.Drawing.Point(49, 309);
             this.guna2Button1.Name = "guna2Button1";
             this.guna2Button1.PressedDepth = 0;
@@ -117,17 +136,32 @@
             // 
             this.drag2.TargetControl = this.pnlMain;
             // 
-            // pnlChangeWindow
+            // guna2Button2
             // 
-            this.pnlChangeWindow.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
-            | System.Windows.Forms.AnchorStyles.Left) 
-            | System.Windows.Forms.AnchorStyles.Right)));
-            this.pnlChangeWindow.BackColor = System.Drawing.Color.Transparent;
-            this.pnlChangeWindow.Location = new System.Drawing.Point(283, 52);
-            this.pnlChangeWindow.Name = "pnlChangeWindow";
-            this.pnlChangeWindow.ShadowDecoration.Parent = this.pnlChangeWindow;
-            this.pnlChangeWindow.Size = new System.Drawing.Size(1044, 833);
-            this.pnlChangeWindow.TabIndex = 3;
+            this.guna2Button2.BackColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.CheckedState.Parent = this.guna2Button2;
+            this.guna2Button2.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.guna2Button2.CustomImages.Parent = this.guna2Button2;
+            this.guna2Button2.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.Font = new System.Drawing.Font("Segoe UI", 10F);
+            this.guna2Button2.ForeColor = System.Drawing.Color.White;
+            this.guna2Button2.HoverState.BorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.HoverState.CustomBorderColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.HoverState.FillColor = System.Drawing.Color.Transparent;
+            this.guna2Button2.HoverState.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.guna2Button2.HoverState.ForeColor = System.Drawing.Color.LightGray;
+            this.guna2Button2.HoverState.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.HoverState.Image")));
+            this.guna2Button2.HoverState.Parent = this.guna2Button2;
+            this.guna2Button2.Image = ((System.Drawing.Image)(resources.GetObject("guna2Button2.Image")));
+            this.guna2Button2.ImageAlign = System.Windows.Forms.HorizontalAlignment.Left;
+            this.guna2Button2.ImageSize = new System.Drawing.Size(25, 25);
+            this.guna2Button2.Location = new System.Drawing.Point(49, 360);
+            this.guna2Button2.Name = "guna2Button2";
+            this.guna2Button2.PressedDepth = 0;
+            this.guna2Button2.ShadowDecoration.Parent = this.guna2Button2;
+            this.guna2Button2.Size = new System.Drawing.Size(189, 45);
+            this.guna2Button2.TabIndex = 1;
+            this.guna2Button2.Text = "Accounts";
             // 
             // frmAdminMain
             // 
@@ -155,5 +189,6 @@
         private Guna.UI2.WinForms.Guna2ControlBox btnClose;
         private Guna.UI2.WinForms.Guna2Button guna2Button1;
         private Guna.UI2.WinForms.Guna2Panel pnlChangeWindow;
+        private Guna.UI2.WinForms.Guna2Button guna2Button2;
     }
 }
